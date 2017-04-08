e72905d659a63269ec548303b223e6351ce04739
CodeSpurt <codespurt@gmail.com>
2017-04-08 12:04:15 +0530

    Initial commit

---
+++ app/src/main/java/com/codespurt/firebasecrashreportingexample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.codespurt.firebasecrashreportingexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.crash.FirebaseCrash;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseCrash.report(new Exception("My first Android non-fatal error"));
+    }
+}
